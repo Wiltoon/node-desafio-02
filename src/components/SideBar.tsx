e1614af2aa6c0480c70725777de914d12de673bc
Wiltoon <wilton00770@gmail.com>
2021-08-18 13:40:02 -0400

    HELO

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
+import { useState } from 'react';
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Array<{
+    genre: GenreResponseProps;
+  }>;
+  selectGenreId: number;
+  setSelectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+
+  function handleClickButton(id: number) {
+    props.setSelectedGenreId(id);
+  }
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
